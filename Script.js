82963624d9cb692784c6c889e7bc6b91682988ef
haydaraliyevjasurbek <jasurbekhaydaraliyev7777@gmail.com>
2025-02-20 13:16:59 +0500

    JS_intro

--- Script.js
+++ Script.js
@@ -5,37 +5,37 @@ console.log(`Меня зовут ${name}`);
 var age = +prompt ("Введите ваш возвраст?")
 console.log(`Мне ${age} лет`);
 
-
+    
 var minus = +prompt("Решите пример: 140 - 97 = ?")
 if (minus==43) {console.log(`Ваш ответ 43 правильный`);
 } 
-else if (minus) {console.log(`Ваш ответ неправильный ${minus} (правильный ответ = 43)`);
+else if (minus) {console.log(`Ваш ответ ${minus} неправильный (правильный ответ = 43)`);
 }
 
 
 var plus = +prompt("Решите пример: 23 + 46 = ?");
 if (plus==69) {console.log(`Ваш ответ 69 правильный`);
 } 
-else if (plus) {console.log(`Ваш ответ неправильный ${plus} (правильный ответ = 69)`);
+else if (plus) {console.log(`Ваш ответ ${plus} неправильный (правильный ответ = 69)`);
 }
 
 
 var devision = +prompt("Решите пример: 999 / 3 = ?");
 if (devision==333) {console.log(`Ваш ответ 333 правильный`);
 } 
-else if (devision) {console.log(`Ваш ответ неправильный ${devision} (правильный ответ = 333)`);
+else if (devision) {console.log(`Ваш ответ ${devision} неправильный (правильный ответ = 333)`);
 }
 
 
 var multiplication = +prompt("Решите пример: 16 * 8 = ?");
 if (multiplication==128) {console.log(`Ваш ответ 128 правильный`);
 } 
-else if (multiplication) {console.log(`Ваш ответ неправильный ${multiplication} (правильный ответ = 128)`);
+else if (multiplication) {console.log(`Ваш ответ ${multiplication} неправильный (правильный ответ = 128)`);
 }
 
 
 var persent = +prompt("Решите пример: 20 % 9 = ?");
 if (persent==2) {console.log(`Ваш ответ 2 правильный`);
 } 
-else if (persent) {console.log(`Ваш ответ неправильный ${persent} (правильный ответ = 2)`);
+else if (persent) {console.log(`Ваш ответ ${persent} неправильный (правильный ответ = 2)`);
 }
